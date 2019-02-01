2b29a27025316716e3980c7e98582ac9c234211f
rheeger <rheeger@gmail.com>
2019-01-31 16:59:00 -0800

    hoping this comes from my account this time

---
+++ NL9/ex02.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	first string
+}
+
+type human interface {
+	speak()
+}
+
+func (a *person) speak() {
+	fmt.Println("AHHHHHHHHH")
+}
+
+func saysomething(h human) {
+	h.speak()
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+	}
+
+	// saysomething(p1)
+	saysomething(&p1)
+
+}
